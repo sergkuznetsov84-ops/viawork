1fda267a6fbe39346adff64206ff359657695c2e
Antigraviti <kuznetsov363sv@gmail.com>
2026-03-06 14:59:33 +0300

    Translate footer legal links in template

--- local/templates/viawork/components/bitrix/menu/bottom_bottom/template.php
+++ local/templates/viawork/components/bitrix/menu/bottom_bottom/template.php
@@ -1,12 +1,20 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 if (empty($arResult)) return;
+
+$legalLabelsMap = array(
+	'Cookie Policy' => 'Политика использования cookie',
+	'Terms & Conditions' => 'Условия использования',
+	'Privacy Policy' => 'Политика конфиденциальности',
+	'Information Security Policy' => 'Политика информационной безопасности',
+);
 ?>
 <div class="legal-links" >
 <?
 foreach($arResult as $arItem):
+	$linkText = isset($legalLabelsMap[$arItem["TEXT"]]) ? $legalLabelsMap[$arItem["TEXT"]] : $arItem["TEXT"];
 ?>
-	<a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+	<a href="<?=$arItem["LINK"]?>"><?=$linkText?></a>
 <?
 endforeach;
 ?>
