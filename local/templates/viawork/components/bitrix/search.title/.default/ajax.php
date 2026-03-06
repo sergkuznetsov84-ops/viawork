2db6a1b31b3ef7b4f2d88034aaab1de07cc68d2c
Antigraviti <kuznetsov363sv@gmail.com>
2026-03-06 16:21:49 +0300

    Translate header search dropdown to Russian

--- local/templates/viawork/components/bitrix/search.title/.default/ajax.php
+++ local/templates/viawork/components/bitrix/search.title/.default/ajax.php
@@ -6,27 +6,27 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 
 if (!empty($arResult['CATEGORIES']) && $arResult['CATEGORIES_ITEMS_EXISTS']):?>
 <div class="search-suggestions" >
-	<div class="title" >
-		<h4>Top Suggestions</h4>
-	</div>
-	<ul>
-		<? foreach ($arResult['CATEGORIES'] as $category_id => $arCategory):?>
-			<? foreach ($arCategory['ITEMS'] as $i => $arItem):?>
-				<?if($arItem['TYPE'] !== 'all'){?>
-				<li>
-					<a href="<?=$arItem['URL'];?>">
-						<i>
+		<div class="title" >
+			<h4><?=defined('LANGUAGE_ID') && LANGUAGE_ID === 'ru' ? 'Популярные запросы' : 'Top Suggestions'?></h4>
+		</div>
+		<ul>
+			<?php foreach ($arResult['CATEGORIES'] as $category_id => $arCategory): ?>
+				<?php foreach ($arCategory['ITEMS'] as $i => $arItem): ?>
+					<?php if ($arItem['TYPE'] !== 'all'): ?>
+					<li>
+						<a href="<?=$arItem['URL'];?>">
+							<i>
 							<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M21.7382 16.1363L17.4328 20.4418C17.331 20.5436 17.1928 20.6018 17.0546 20.6018C16.9164 20.6018 16.7782 20.5509 16.6764 20.4418C16.4655 20.2309 16.4655 19.8891 16.6764 19.6781L20.0655 16.2891H9.45459V15.2127H20.0582L16.6691 11.8309C16.4582 11.62 16.4582 11.2781 16.6691 11.0672C16.88 10.8563 17.2219 10.8563 17.4328 11.0672L21.7382 15.3727C21.9491 15.5836 21.9491 15.9254 21.7382 16.1363Z" fill="#373F43"></path>
 							</svg>
 						</i>
-						<span><?=$arItem['NAME'];?></span>
-					</a>
-				</li>
-				<?}?>
-			<?php endforeach;?>
-		<?php endforeach;?>
-	</ul>
+							<span><?=$arItem['NAME'];?></span>
+						</a>
+					</li>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			<?php endforeach; ?>
+		</ul>
 </div>
 <div class="title-search-fader"></div>
 <?php endif;
--- local/templates/viawork/components/bitrix/search.title/.default/template.php
+++ local/templates/viawork/components/bitrix/search.title/.default/template.php
@@ -29,6 +29,9 @@ if ($CONTAINER_ID == '')
 	$CONTAINER_ID = 'title-search';
 }
 $CONTAINER_ID = CUtil::JSEscape($CONTAINER_ID);
+$isRussian = defined('LANGUAGE_ID') && LANGUAGE_ID === 'ru';
+$searchLabel = $isRussian ? 'Поиск' : 'Search';
+$searchPlaceholder = $isRussian ? 'Поиск...' : 'Search ...';
 
 if ($arParams['SHOW_INPUT'] !== 'N'):?>
 	<div class="search" ><!---->
@@ -58,23 +61,23 @@ if ($arParams['SHOW_INPUT'] !== 'N'):?>
 							<svg xmlns="http://www.w3.org/2000/svg" width="21" height="18" viewBox="0 0 21 18" fill="none">
 							<path d="M0.26699 8.28649L7.53398 1.0195C7.70583 0.847646 7.93906 0.749444 8.17229 0.749444C8.40553 0.749444 8.63876 0.83537 8.81061 1.0195C9.16659 1.37548 9.16659 1.95242 8.81061 2.30841L3.09031 8.02871L21 8.02871L21 9.84545L3.10259 9.84545L8.82288 15.5535C9.17887 15.9095 9.17887 16.4864 8.82288 16.8424C8.4669 17.1984 7.88996 17.1984 7.53398 16.8424L0.266989 9.5754C-0.0889943 9.21941 -0.0889942 8.64247 0.26699 8.28649Z" fill="#25282A"></path>
 							</svg>
-						</i>
-						Search                
-					</a>
-				</div>
-				<div class="row" >
-					<div class="col-lg-6 offset-lg-3"  id="<?php echo $CONTAINER_ID?>">
-					<form action="<?php echo $arResult['FORM_ACTION']?>">
-						<button  name="s" type="submit" value="<?=GetMessage('CT_BST_SEARCH_BUTTON');?>>
-						<i>
-							<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M12.0991 12.0985L17 16.9994" stroke="#ACB4B7" stroke-width="1.50796" stroke-linecap="round" stroke-linejoin="round"></path>
+							</i>
+							<?=$searchLabel?>                
+						</a>
+					</div>
+					<div class="row" >
+						<div class="col-lg-6 offset-lg-3"  id="<?php echo $CONTAINER_ID?>">
+						<form action="<?php echo $arResult['FORM_ACTION']?>">
+							<button  name="s" type="submit" value="<?=GetMessage('CT_BST_SEARCH_BUTTON')?>">
+							<i>
+								<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M12.0991 12.0985L17 16.9994" stroke="#ACB4B7" stroke-width="1.50796" stroke-linecap="round" stroke-linejoin="round"></path>
 							<path d="M14.0062 7.50685C14.0062 11.0958 11.0958 14.0062 7.50685 14.0062C3.9179 14.0062 1 11.0958 1 7.50685C1 3.9179 3.91036 1 7.49931 1C11.0883 1 13.9986 3.91037 13.9986 7.50685H14.0062Z" stroke="#ACB4B7" stroke-width="1.50796" stroke-linecap="round" stroke-linejoin="round"></path>
 							</svg>
-						</i>
-						</button>
-						<input  id="<?php echo $INPUT_ID?>" type="text" name="q" value="" placeholder="Search ..." class="search-input-dropdown"  autocomplete="off">
-					</form>
+							</i>
+							</button>
+							<input  id="<?php echo $INPUT_ID?>" type="text" name="q" value="" placeholder="<?=$searchPlaceholder?>" class="search-input-dropdown"  autocomplete="off">
+						</form>
 					
 					</div>
 				</div>
