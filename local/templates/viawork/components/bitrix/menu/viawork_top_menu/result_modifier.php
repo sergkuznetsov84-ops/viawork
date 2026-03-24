4559efe8734f758474e0cbc1d8a137724f401b23
AntonSergeevich34 <zlytoks@gmail.com>
2026-03-24 15:42:51 +0300

    Правка Мобильного меню, новая логика + добавление меню 3го уровня + правка стилей для мобильной версии: шрифт ссылок в подвале, шрифт для блока Материалы на стр. Устойчивое развитие

--- local/templates/viawork/components/bitrix/menu/viawork_top_menu/result_modifier.php
+++ local/templates/viawork/components/bitrix/menu/viawork_top_menu/result_modifier.php
@@ -1,220 +1,100 @@
 <?
-if (!CModule::IncludeModule("iblock")) {
-    die("Модуль информационных блоков не установлен");
-}
+if (!CModule::IncludeModule("iblock")) return;
 
 $iblockId = 2;
-$menuElementSectionId = 5;
+$ID_PRODUCTS = 5; 
+$ID_PARTNERS = 6; 
+$ID_SUBSECTIONS = [7, 8]; 
 
 if (!function_exists('buildCatalogMenuUrl')) {
-    function buildCatalogMenuUrl($sectionCodePath = '', $elementCode = '')
-    {
+    function buildCatalogMenuUrl($sectionCodePath = '', $elementCode = '') {
         $parts = array('/all-products');
-
-        if ($sectionCodePath !== '') {
-            $parts[] = trim($sectionCodePath, '/');
-        }
-
-        if ($elementCode !== '') {
-            $parts[] = trim($elementCode, '/');
-        }
-
+        if ($sectionCodePath !== '') $parts[] = trim($sectionCodePath, '/');
+        if ($elementCode !== '') $parts[] = trim($elementCode, '/');
         return implode('/', $parts) . '/';
     }
 }
 
 if (!function_exists('buildSectionCodePath')) {
-    function buildSectionCodePath($sectionId, array &$sectionsById, array &$cache = array())
-    {
-        if (isset($cache[$sectionId])) {
-            return $cache[$sectionId];
-        }
-
-        if (!isset($sectionsById[$sectionId])) {
-            return '';
-        }
-
+    function buildSectionCodePath($sectionId, array &$sectionsById, array &$cache = array()) {
+        if (isset($cache[$sectionId])) return $cache[$sectionId];
+        if (!isset($sectionsById[$sectionId])) return '';
         $section = $sectionsById[$sectionId];
         $parts = array();
-
         if ((int)$section['IBLOCK_SECTION_ID'] > 0) {
             $parentPath = buildSectionCodePath((int)$section['IBLOCK_SECTION_ID'], $sectionsById, $cache);
-            if ($parentPath !== '') {
-                $parts[] = $parentPath;
-            }
+            if ($parentPath !== '') $parts[] = $parentPath;
         }
-
-        if ($section['CODE'] !== '') {
-            $parts[] = $section['CODE'];
-        }
-
+        if ($section['CODE'] !== '') $parts[] = $section['CODE'];
         $cache[$sectionId] = implode('/', $parts);
-
         return $cache[$sectionId];
     }
 }
 
-// if (!function_exists('getIBlockSectionTree')) {
-//     function getIBlockSectionTree($iblockId)
-//     {
-//         $sectionsById = array();
-//         $codePathCache = array();
-
-//         $dbSections = CIBlockSection::GetList(
-//             array('LEFT_MARGIN' => 'ASC'),
-//             array(
-//                 'IBLOCK_ID' => $iblockId,
-//                 'ACTIVE' => 'Y',
-//                 '!ID' => array(32, 248),
-//             ),
-//             false,
-//             array('ID', 'NAME', 'CODE', 'DEPTH_LEVEL', 'IBLOCK_SECTION_ID', 'UF_ICON', 'UF_SHOW_IN_MENU')
-//         );
-
-//         while ($arSection = $dbSections->GetNext()) {
-//             $arSection['ICON'] = null;
-//             if (!empty($arSection['UF_ICON'])) {
-//                 $arSection['ICON'] = CFile::GetFileArray($arSection['UF_ICON']);
-//             }
-
-//             $sectionsById[(int)$arSection['ID']] = $arSection;
-//         }
-
-//         $tree = array();
-//         foreach ($sectionsById as $sectionId => $section) {
-//             $codePath = buildSectionCodePath($sectionId, $sectionsById, $codePathCache);
-//             $sectionUrl = $codePath !== '' ? buildCatalogMenuUrl($codePath) : '/all-products/';
-
-//             if ((int)$section['IBLOCK_SECTION_ID'] === 0) {
-//                 $tree[$sectionId] = array(
-//                     'ID' => $sectionId,
-//                     'NAME' => $section['NAME'],
-//                     'URL' => $sectionUrl,
-//                     'ICON' => $section['ICON'] ? $section['ICON']['SRC'] : '',
-//                     'SUB_SECTIONS' => array(),
-//                 );
-//                 continue;
-//             }
-
-//             $parentId = (int)$section['IBLOCK_SECTION_ID'];
-//             if ((int)$section['UF_SHOW_IN_MENU'] === 1 && isset($tree[$parentId])) {
-//                 $tree[$parentId]['SUB_SECTIONS'][] = array(
-//                     'NAME' => $section['NAME'],
-//                     'URL' => $sectionUrl,
-//                 );
-//             }
-//         }
-
-//         return array($tree, $sectionsById, $codePathCafche);
-//     }
-// }
-
-if (!function_exists('getIBlockSectionTree')) {
-    function getIBlockSectionTree($iblockId)
-    {
-        $sectionsById = array();
-        $codePathCache = array();
-
-        $dbSections = CIBlockSection::GetList(
-            array('LEFT_MARGIN' => 'ASC'),
-            array(
-                'IBLOCK_ID' => $iblockId,
-                'ACTIVE' => 'Y',
-                '!ID' => array(32, 248, 253, 254),
-            ),
-            false,
-            // Добавили UF_LINK в массив ниже
-            array('ID', 'NAME', 'CODE', 'DEPTH_LEVEL', 'IBLOCK_SECTION_ID', 'UF_ICON', 'UF_SHOW_IN_MENU', 'UF_LINK')
-        );
-
-        while ($arSection = $dbSections->GetNext()) {
-            $arSection['ICON'] = null;
-            if (!empty($arSection['UF_ICON'])) {
-                $arSection['ICON'] = CFile::GetFileArray($arSection['UF_ICON']);
-            }
-
-            $sectionsById[(int)$arSection['ID']] = $arSection;
-        }
-
-        $tree = array();
-        foreach ($sectionsById as $sectionId => $section) {
-            $codePath = buildSectionCodePath($sectionId, $sectionsById, $codePathCache);
-            $sectionUrl = $codePath !== '' ? buildCatalogMenuUrl($codePath) : '/all-products/';
-
-            if ((int)$section['IBLOCK_SECTION_ID'] === 0) {
-                $tree[$sectionId] = array(
-                    'ID' => $sectionId,
-                    'NAME' => $section['NAME'],
-                    'URL' => $sectionUrl,
-                    'ICON' => $section['ICON'] ? $section['ICON']['SRC'] : '',
-                    'SUB_SECTIONS' => array(),
-                );
-                continue;
-            }
-
-            $parentId = (int)$section['IBLOCK_SECTION_ID'];
-            if ((int)$section['UF_SHOW_IN_MENU'] === 1 && isset($tree[$parentId])) {
-                
-                // ЛОГИКА ДЛЯ ID=6:
-                // Если родитель раздела ("Переговорные кабинки") имеет ID 6 и заполнено поле UF_LINK, берем его
-                $finalUrl = $sectionUrl;
-                if ($parentId === 6 && !empty($section['UF_LINK'])) {
-                    $finalUrl = $section['UF_LINK'];
-                }
-
-                $tree[$parentId]['SUB_SECTIONS'][] = array(
-                    'NAME' => $section['NAME'],
-                    'URL' => $finalUrl, // Используем итоговый URL
-                );
-            }
-        }
-
-        return array($tree, $sectionsById, $codePathCache);
-    }
-}
-
-list($sectionTree, $sectionsById, $codePathCache) = getIBlockSectionTree($iblockId);
-
-$res = CIBlockElement::GetList(
+$sectionsById = array();
+$codePathCache = array();
+$dbSections = CIBlockSection::GetList(
     array('SORT' => 'ASC'),
-    array(
-        'IBLOCK_ID' => $iblockId,
-        'SECTION_ID' => $menuElementSectionId,
-        'ACTIVE' => 'Y',
-    ),
+    array('IBLOCK_ID' => $iblockId, 'ACTIVE' => 'Y', '!ID' => array(32, 248, 253, 254)),
     false,
-    false,
-    array(
-        'ID',
-        'NAME',
-        'CODE',
-        'IBLOCK_SECTION_ID',
-        'DETAIL_PAGE_URL',
-        'PROPERTY_SHOW_IN_MENU',
-    )
+    array('ID', 'NAME', 'CODE', 'IBLOCK_SECTION_ID', 'UF_ICON', 'UF_SHOW_IN_MENU', 'UF_LINK')
 );
 
-$elementsArray = array();
-while ($ob = $res->GetNext()) {
-    if ($ob['PROPERTY_SHOW_IN_MENU_VALUE'] !== 'Y') {
-        continue;
+while ($arSect = $dbSections->GetNext()) {
+    $arSect['ICON_SRC'] = '';
+    if (!empty($arSect['UF_ICON'])) {
+        $file = CFile::GetFileArray($arSect['UF_ICON']);
+        $arSect['ICON_SRC'] = $file['SRC'];
     }
-
-    $sectionId = (int)$ob['IBLOCK_SECTION_ID'];
-    $sectionCodePath = $sectionId > 0 ? buildSectionCodePath($sectionId, $sectionsById, $codePathCache) : '';
-    $elementUrl = ($sectionCodePath !== '' && $ob['CODE'] !== '')
-        ? buildCatalogMenuUrl($sectionCodePath, $ob['CODE'])
-        : $ob['DETAIL_PAGE_URL'];
-
-    $elementsArray[] = array(
-        'NAME' => $ob['NAME'],
-        'URL' => $elementUrl,
-    );
+    $sectionsById[(int)$arSect['ID']] = $arSect;
 }
 
-if (!empty($elementsArray) && isset($sectionTree[$menuElementSectionId])) {
-    $sectionTree[$menuElementSectionId]['SUB_SECTIONS'] = $elementsArray;
+// Элементы для раздела 5
+$elementsFor5 = [];
+$resElements = CIBlockElement::GetList(
+    array('SORT' => 'ASC'),
+    array('IBLOCK_ID' => $iblockId, 'SECTION_ID' => $ID_PRODUCTS, 'ACTIVE' => 'Y', 'PROPERTY_SHOW_IN_MENU_VALUE' => 'Y'),
+    false, false,
+    array('ID', 'NAME', 'CODE', 'IBLOCK_SECTION_ID', 'DETAIL_PAGE_URL')
+);
+while ($obEl = $resElements->GetNext()) {
+    $sCodePath = buildSectionCodePath((int)$obEl['IBLOCK_SECTION_ID'], $sectionsById, $codePathCache);
+    $elementsFor5[] = [
+        'NAME' => $obEl['NAME'],
+        'URL' => buildCatalogMenuUrl($sCodePath, $obEl['CODE'])
+    ];
 }
 
-$arResult['CATALOG_SECTIONS'] = array_values($sectionTree);
+$arResult['CATALOG_DROPDOWN'] = [];
+foreach ($sectionsById as $id => $sect) {
+    if ((int)$sect['IBLOCK_SECTION_ID'] == 0) {
+        $subItems = [];
+        if ($id == $ID_PRODUCTS) $subItems = $elementsFor5;
+        
+        if ($id == $ID_PARTNERS || in_array($id, $ID_SUBSECTIONS)) {
+            foreach ($sectionsById as $subId => $subSect) {
+                if ((int)$subSect['IBLOCK_SECTION_ID'] == $id && $subSect['UF_SHOW_IN_MENU'] == 1) {
+                    $sCode = buildSectionCodePath($subId, $sectionsById, $codePathCache);
+                    // Ссылка подраздела (для 6-го раздела берем UF_LINK)
+                    $url = ($id == $ID_PARTNERS && !empty($subSect['UF_LINK'])) ? $subSect['UF_LINK'] : buildCatalogMenuUrl($sCode);
+                    $subItems[] = ['NAME' => $subSect['NAME'], 'URL' => $url, 'TARGET' => ($id == $ID_PARTNERS) ? '_blank' : ''];
+                }
+            }
+        }
+
+        // Ссылка САМОГО раздела (для 6-го раздела берем UF_LINK)
+        $rootUrl = ($id == $ID_PARTNERS && !empty($sect['UF_LINK'])) 
+                   ? $sect['UF_LINK'] 
+                   : buildCatalogMenuUrl(buildSectionCodePath($id, $sectionsById, $codePathCache));
+
+        $arResult['CATALOG_DROPDOWN'][] = [
+            'ID' => $id,
+            'NAME' => $sect['NAME'],
+            'URL' => $rootUrl,
+            'ICON' => $sect['ICON_SRC'],
+            'SUB_SECTIONS' => $subItems,
+            'TARGET' => ($id == $ID_PARTNERS) ? '_blank' : ''
+        ];
+    }
+}
 ?>
