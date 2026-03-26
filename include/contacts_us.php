d0a6c57c598744722c8330305dd30cde9c5a8b7d
AntonSergeevich34 <zlytoks@gmail.com>
2026-03-26 17:33:13 +0300

    изменил верстку на вызов компонента для страницы Контакты - блок Свяжитесь с нами

--- include/contacts_us.php
+++ include/contacts_us.php
@@ -1,74 +1,69 @@
-<section class="comp-93 style-1">
-  <div class="comp-93-wrapper">
-    <div class="container">
-      <div class="comp-93-main">
-        <div class="comp-93-head">
-          <div class="subtitle fadeInUp-scroll visible" data-delay="200">
-            <h4>СВЯЗАТЬСЯ С НАМИ</h4>
-          </div>
-          <div class="title fadeInUp-scroll visible" data-delay="250">
-            <h2>Мы рядом на каждом этапе</h2>
-          </div>
-        </div>
-        <div class="comp-93-content fadeInUp-scroll visible" data-delay="400">
-          <div class="" style="
-    display: flex;
-    justify-content: center;
-    align-items: center;
-">
-    
-            
-            <div class="col-lg-4">
-              <div class="card">
-                <div class="card-media">
-                  <span>
-                    <source media="(max-width: 991px)" srcset="<?=SITE_TEMPLATE_PATH.'/src/img/comp-93-gorsel-wp.jpg'?>">
-                    <source media="(min-width: 992px)" srcset="<?=SITE_TEMPLATE_PATH.'/src/img/comp-93-gorsel-wp.jpg'?>">
-                    <img src="<?=SITE_TEMPLATE_PATH.'/src/img/comp-93-gorsel-wp.jpg'?>" class="sp-no-webp" alt="">
-                  </span>
-                </div>
-                <div class="card-content">
-                  <div class="text">
-                    <p>Давайте спланируем ваш рабочий офис вместе</p>
-                  </div>
-                  <div class="action">
-                    <a href="https://nurus.com/contact/" class="btn btn-white btn-rnd-full btn-medium btn-gradient">
-                      Связаться с нами
-                      <i></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            
-            
-            <div class="col-lg-4">
-              <div class="card">
-                <div class="card-media">
-                  <span>
-                    <source media="(max-width: 991px)" srcset="https://nurus-storage.s3.amazonaws.com/wp-content/uploads/2025/08/26105104/comp-93-calma.jpg">
-                    <source media="(min-width: 992px)" srcset="https://nurus-storage.s3.amazonaws.com/wp-content/uploads/2025/08/26105104/comp-93-calma.jpg">
-                    <img src="<?=SITE_TEMPLATE_PATH.'/src/img/comp-93-calma.jpg'?>" class="sp-no-webp" alt="">
-                  </span>
-                </div>
-                <div class="card-content">
-                  <div class="text">
-                    <p>Создано для более комфортного рабочего опыта</p>
-                  </div>
-                  <div class="action">
-                    <a href="https://calma.nurus.com/" class="btn btn-blue btn-regular btn-rnd-full btn-blue">
-                      Calma
-                      <i></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<?$APPLICATION->IncludeComponent(
+  "bitrix:news.list", 
+  "contact_us", 
+  array(
+    "DISPLAY_DATE" => "Y",
+    "DISPLAY_NAME" => "Y",
+    "DISPLAY_PICTURE" => "Y",
+    "DISPLAY_PREVIEW_TEXT" => "Y",
+    "AJAX_MODE" => "Y",
+    "IBLOCK_TYPE" => "content",
+    "IBLOCK_ID" => "30",
+    "NEWS_COUNT" => "3",
+    "SORT_BY1" => "ID",
+    "SORT_ORDER1" => "ASC",
+    "SORT_BY2" => "SORT",
+    "SORT_ORDER2" => "ASC",
+    "FILTER_NAME" => "",
+    "FIELD_CODE" => array(
+      0 => "ID",
+      1 => "",
+    ),
+    "PROPERTY_CODE" => array(
+      0 => "",
+      1 => "DESCRIPTION",
+      2 => "",
+    ),
+    "CHECK_DATES" => "Y",
+    "DETAIL_URL" => "",
+    "PREVIEW_TRUNCATE_LEN" => "",
+    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+    "SET_TITLE" => "N",
+    "SET_BROWSER_TITLE" => "N",
+    "SET_META_KEYWORDS" => "N",
+    "SET_META_DESCRIPTION" => "N",
+    "SET_LAST_MODIFIED" => "N",
+    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+    "ADD_SECTIONS_CHAIN" => "Y",
+    "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+    "PARENT_SECTION" => "",
+    "PARENT_SECTION_CODE" => "",
+    "INCLUDE_SUBSECTIONS" => "Y",
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "3600",
+    "CACHE_FILTER" => "Y",
+    "CACHE_GROUPS" => "Y",
+    "DISPLAY_TOP_PAGER" => "N",
+    "DISPLAY_BOTTOM_PAGER" => "N",
+    "PAGER_TITLE" => "",
+    "PAGER_SHOW_ALWAYS" => "Y",
+    "PAGER_TEMPLATE" => "",
+    "PAGER_DESC_NUMBERING" => "Y",
+    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+    "PAGER_SHOW_ALL" => "Y",
+    "PAGER_BASE_LINK_ENABLE" => "Y",
+    "SET_STATUS_404" => "Y",
+    "SHOW_404" => "Y",
+    "MESSAGE_404" => "",
+    "PAGER_BASE_LINK" => "",
+    "PAGER_PARAMS_NAME" => "arrPager",
+    "AJAX_OPTION_JUMP" => "N",
+    "AJAX_OPTION_STYLE" => "Y",
+    "AJAX_OPTION_HISTORY" => "N",
+    "AJAX_OPTION_ADDITIONAL" => "",
+    "COMPONENT_TEMPLATE" => "about_blog_slider",
+    "STRICT_SECTION_CHECK" => "N",
+    "FILE_404" => ""
+  ),
+  false
+);?>
